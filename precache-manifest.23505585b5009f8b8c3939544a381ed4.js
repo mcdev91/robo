76199057a8c16752822abfd9d3af3b308f3ad2ce
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-01-29 22:31:20 +0200

    Updates

---
+++ precache-manifest.23505585b5009f8b8c3939544a381ed4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a206fbbc8038b54860ee0e711cd03f8",
+    "url": "//robo/index.html"
+  },
+  {
+    "revision": "2d0ba42ab2945df279da",
+    "url": "//robo/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "bdb305039962bc5da249",
+    "url": "//robo/static/css/main.927cebaf.chunk.css"
+  },
+  {
+    "revision": "2d0ba42ab2945df279da",
+    "url": "//robo/static/js/2.d21f8b18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "//robo/static/js/2.d21f8b18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bdb305039962bc5da249",
+    "url": "//robo/static/js/main.695dfe86.chunk.js"
+  },
+  {
+    "revision": "542e940d3a8a8dcb9af0",
+    "url": "//robo/static/js/runtime-main.acd20d6c.js"
+  }
+]);
