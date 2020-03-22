e38c17b8b07fdfc3478f2e52e27e2e0d9a3defc8
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-03-22 21:12:10 +0200

    rebuilded with redux

--- src/reducers.js
+++ src/reducers.js
@@ -1,15 +1,39 @@
-import { CHANGE_SERACH_FIELD } from './constants.js';
+import {
+    CHANGE_SERACH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCES,
+    REQUEST_ROBOTS_FAILED
+} from './constants.js';
 
-const initialState = {
+const initialStateSearch = {
     searchField: ''
 }
 
-export const searchRobots = (state = initialState, action = {}) => {
+export const searchRobots = (state = initialStateSearch, action = {}) => {
     // console.log(action.type)
     switch (action.type) {
         case CHANGE_SERACH_FIELD:
             return Object.assign({}, state, { searchField: action.payload });
         default:
             return state;
+    }
+}
+
+const initialStateRobots = {
+    isPending: false,
+    robots: [],
+    error: ''
+}
+
+export const requestRobots = (state = initialStateRobots, action = {}) => {
+    switch (action.type) {
+        case REQUEST_ROBOTS_PENDING:
+            return Object.assign({}, state, { isPending: true })
+        case REQUEST_ROBOTS_SUCCES:
+            return Object.assign({}, state, { robots: action.payload, isPending: false })
+        case REQUEST_ROBOTS_FAILED:
+            return Object.assign({}, state, { error: action.payload, isPending: false })
+        default:
+            return state
     }
 }
